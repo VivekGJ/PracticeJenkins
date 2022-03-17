1c23c9bffd9ece01ee1c190128ef1a01583b7f37
VIVEK <VIVEK@DESKTOP-FUABKC3>
2022-03-17 10:34:20 +0530

    vivek read data commit for other

--- src/test/java/demotest/ReadDataTest.java
+++ src/test/java/demotest/ReadDataTest.java
@@ -11,6 +11,10 @@ public class ReadDataTest
 		System.out.println(BROWSER);
 		String URL = System.getProperty("url");
 		System.out.println(URL);
+		String USERNAME = System.getProperty("username");
+		System.out.println(USERNAME);
+		String PASSWORD = System.getProperty("password");
+		System.out.println(PASSWORD);
 	}
 
 }
